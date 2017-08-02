fab871d5d9f0f0e1d71dd131b6e141cd77994d69
Thomas Tuts <thomas@thomastuts.com>
2017-08-02 15:08:14 +0200

    Add basic test file to demonstrate usage of Jest

---
+++ src/index.test.js
@@ -0,0 +1,9 @@
+describe('Our first tests', function () {
+  it('should work', function () {
+    expect(true).toEqual(true);
+  });
+
+  it('should work again', function () {
+    expect(true).toEqual(true);
+  });
+});

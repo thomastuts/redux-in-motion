9f857133165793d7d18ab4aef5b5c18f37e0e672
Thomas Tuts <thomas@thomastuts.com>
2017-08-02 15:40:24 +0200

    Add tests exploring the basic functionality of expect() assertions

--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,22 @@
-describe('Our first tests', function () {
-  it('should work', function () {
-    expect(true).toEqual(true);
-  });
+it('should check for equal values', function () {
+  expect(5).toEqual(5);
+});
 
-  it('should work again', function () {
-    expect(true).toEqual(true);
-  });
+it('should check for equal values using deep equality', function () {
+  expect({ foo: 5 }).toEqual({ foo: 5 });
+  expect([1, 2, 3]).toEqual([1, 2, 3]);
+});
+
+it('should check that an array contains a specific value', function () {
+  expect([1, 2, 3]).toContain(2);
+});
+
+it('should check that an array contains a specific object/array', function () {
+  expect([{ foo: 5 }, { foo: 6 }]).toContainEqual({ foo: 6 });
+});
+
+it('should support spies', function () {
+  const spy = jest.fn();
+  spy('foo');
+  expect(spy).toHaveBeenCalledWith('foo');
 });

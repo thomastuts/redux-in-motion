44e52bb6671bd452ec81e66935b84118ca01b0ae
Thomas Tuts <thomas@thomastuts.com>
2017-10-10 14:12:21 +0200

    Add Redux DevTools integration; remove logger middleware

--- src/middleware/logger.js
+++
@@ -1,6 +0,0 @@
-export default store => next => action => {
-  const result = next(action);
-  console.log('Action:', action);
-  console.log('State:', store.getState());
-  return result;
-}
--- src/store.js
+++ src/store.js
@@ -1,10 +1,13 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import { reducer as freezer } from './ducks/freezer';
-import logger from './middleware/logger';
 
 const rootReducer = combineReducers({
   freezer,
 });
 
-export default createStore(rootReducer, applyMiddleware(thunk, logger));
+export default createStore(rootReducer, composeWithDevTools(
+  applyMiddleware(thunk)
+));

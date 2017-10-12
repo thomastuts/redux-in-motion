d9439182a4c8df7a1b3d87d55b1097cf2e6b8606
Thomas Tuts <thomas@thomastuts.com>
2017-10-12 16:57:17 +0200

    Add temperature measurements & restocking functionality

--- src/components/Freezer/Freezer.js
+++ src/components/Freezer/Freezer.js
@@ -1,34 +1,68 @@
 import React, { Component } from 'react';
 
+import { actions } from '../../ducks/freezer';
 import store from '../../store';
+import * as FLAVORS from '../../constants/flavors';
 
 import Panel from '../Panel/Panel';
+import Button from '../Button/Button';
 import FreezerFlavor from '../FreezerFlavor/FreezerFlavor';
 
 class Freezer extends Component {
   state = {
     flavors: store.getState().freezer.flavors,
+    temperature: store.getState().freezer.temperature,
   };
 
   componentDidMount() {
     this.unsubscribe = store.subscribe(() => {
       this.setState({
         flavors: store.getState().freezer.flavors,
+        temperature: store.getState().freezer.temperature,
       });
     });
+
+    setInterval(() => {
+      const randomTemperature = -Math.round(Math.random() * 10);
+      store.dispatch(actions.updateTemperature(randomTemperature));
+    }, 2000);
   }
 
   componentWillUnmount() {
     this.unsubscribe();
   }
 
+  handleClickRestock = (flavorName) => {
+    const amount = parseInt(window.prompt(`Enter amount to restock ${flavorName}`));
+
+    if (!isNaN(amount)) {
+      store.dispatch(actions.addProductToFreezer(flavorName, amount));
+    }
+  };
+
+  handleClickAddProduct = () => {
+    const allAvailableFlavors = Object.keys(FLAVORS);
+    const flavorName = window.prompt(`Enter flavor name to restock. (Choose from: ${allAvailableFlavors.join(', ')})`);
+
+    if (FLAVORS[flavorName]) {
+      this.handleClickRestock(flavorName);
+    }
+  };
+
   render() {
     const flavors = Object.keys(this.state.flavors).map(flavorName => (
-      <FreezerFlavor key={flavorName} flavorName={flavorName} scoops={this.state.flavors[flavorName]} />
+      <FreezerFlavor
+        key={flavorName}
+        onClickRestock={() => this.handleClickRestock(flavorName)}
+        flavorName={flavorName}
+        scoops={this.state.flavors[flavorName]}
+      />
     ));
 
     return (
-      <Panel title="Freezer (°0C)">
+      <Panel title={`Freezer (°${this.state.temperature || 0}C)`}>
+        <Button label="Add product" onClick={this.handleClickAddProduct} />
+        <br/>
         {flavors}
       </Panel>
     );
--- src/components/FreezerFlavor/FreezerFlavor.js
+++ src/components/FreezerFlavor/FreezerFlavor.js
@@ -4,14 +4,14 @@ import Button from '../Button/Button';
 
 import './FreezerFlavor.css';
 
-const FreezerFlavor = ({ flavorName, scoops }) => {
+const FreezerFlavor = ({ flavorName, scoops, onClickRestock }) => {
   return (
     <div className="freezer-flavor">
       <div className="freezer-flavor__inner">
         <div className="freezer-flavor__scoops-counter">{scoops}</div>
         <div className="freezer-flavor__name">{flavorName}</div>
       </div>
-      <Button className="freezer-flavor__restock-button" label="Restock"/>
+      <Button onClick={onClickRestock} className="freezer-flavor__restock-button" label="Restock"/>
     </div>
   );
 };
